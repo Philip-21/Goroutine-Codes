8e6d21e4780798fb72d307e7648a219ab7c43750
Philip-21 <philipuzomaobiora@gmail.com>
2023-01-08 04:34:26 +0100

    more items

--- mutexes/example-2/main.go
+++ mutexes/example-2/main.go
@@ -9,7 +9,7 @@ import (
 
 var wg sync.WaitGroup
 
-type income struct {
+type Income struct {
 	Source string
 	Amount int
 }
@@ -23,14 +23,32 @@ func main() {
 	fmt.Println()
 
 	//define weekly revenue
-	incomes := []income{
+	incomes := []Income{
 		{Source: "Main job", Amount: 500},
 		{Source: "Crypto", Amount: 10},
 		{Source: "Part time job", Amount: 50},
 		{Source: "Investments", Amount: 100},
 	}
+	wg.Add(len(incomes))
 
 	//loop through 52 weeks and print out how much; keep a runnig total
+	for i, income := range incomes { ///looping through income with the range of incomes
+
+		go func(i int, income Income) {
+			defer wg.Done()
+			//loop through all 52 weeks
+			for week := 1; week <= 52; week++ {
+				temp := bankBalance   //current bank balance
+				temp += income.Amount //add to the current income for the current week
+				bankBalance = temp    //add temp to the bank balance
+				fmt.Printf("On Week %d, you earned $%d.00 from %s\n", week,
+					income.Amount, income.Source)
+			}
+		}(i, income)
+	}
+	wg.Wait()
 
 	//print final balance
+	fmt.Printf("Final Bank Balance: $%d.00", bankBalance)
+	fmt.Println()
 }
