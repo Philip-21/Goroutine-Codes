331318cf5feb57413bb3b6f18fa61f95bd7175a7
Philip-21 <philipuzomaobiora@gmail.com>
2023-01-18 23:06:39 +0100

    channels irst example

---
+++ channels/example-1/main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"fmt"
+	"strings"
+	"sync"
+)
+
+func shout(ping <-chan string, pong chan<- string) {
+	defer wg.Done()
+	for {
+		//accept a value from ping
+		s := <-ping //ping receives from the channel
+
+		///pong sends data to the channel received from ping defined with the variable s
+		pong <- fmt.Sprintf("%s!!", strings.ToUpper(s))
+	}
+}
+
+var wg sync.WaitGroup
+
+func main() {
+
+	ping := make(chan string)
+	pong := make(chan string)
+
+	go shout(ping, pong)
+
+	fmt.Println("Type Something and Press ENTER ,enter Q to quit ")
+	for {
+		//print a prompt
+		fmt.Print("->")
+		//get user input
+		var userinput string
+		//accepting user inputs omly in uppercase
+		_, _ = fmt.Scanln(&userinput)
+		if userinput == strings.ToLower("q") {
+			break //quit
+		}
+
+		//send data to ping channel(ping  receivin the data userinput )
+		ping <- userinput
+		//wait on the response
+		response := <-pong //(pong receives the data from ping , then sends the data to the channel)
+		fmt.Println("Response", response)
+
+	}
+	fmt.Println("All done, Closing Channels")
+	close(ping)
+	close(pong)
+
+}
---
+++ channels/readme.md
@@ -0,0 +1,5 @@
+Channels is a means communicating btwn two or more goroutines 
+Channels sends data btwn goroutines 
+Channels can  be buffered(more than one things in channel) or unbuffered( one thing in a channel)
+Channels typically only accepts a given type of interface 
+Once you are done with a channel you  must close it
